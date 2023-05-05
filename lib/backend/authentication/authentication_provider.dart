36c092395a19eb862455480506c71d4926514781
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2023-05-05 16:19:34 +0530

    added userModel in AuthenticationProvider

--- lib/backend/authentication/authentication_provider.dart
+++ lib/backend/authentication/authentication_provider.dart
@@ -1,4 +1,5 @@
 import 'package:club_model/club_model.dart';
+import 'package:club_model/models/user/data_model/user_model.dart';
 
 class AuthenticationProvider extends CommonProvider {
   AuthenticationProvider() {
@@ -14,11 +15,16 @@ class AuthenticationProvider extends CommonProvider {
       value: "",
       notify: notify,
     );
+    userModel = CommonProviderPrimitiveParameter<UserModel?>(
+      value: null,
+      notify: notify,
+    );
   }
 
   late CommonProviderPrimitiveParameter<User?> firebaseUser;
   late CommonProviderPrimitiveParameter<String> userId;
   late CommonProviderPrimitiveParameter<String> mobileNumber;
+  late CommonProviderPrimitiveParameter<UserModel?> userModel;
 
   void setAuthenticationDataFromFirebaseUser({
     User? firebaseUser,
@@ -33,5 +39,6 @@ class AuthenticationProvider extends CommonProvider {
     firebaseUser.set(value: null, isNotify: false);
     userId.set(value: "", isNotify: false);
     mobileNumber.set(value: "", isNotify: isNotify);
+    userModel.set(value: null, isNotify: isNotify);
   }
 }
