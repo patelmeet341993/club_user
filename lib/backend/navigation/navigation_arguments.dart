7147319ac8ac049299e6d2c5996e6dd65cb0ad89
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2023-05-09 15:12:42 +0530

    added EditProfileScreen

--- lib/backend/navigation/navigation_arguments.dart
+++ lib/backend/navigation/navigation_arguments.dart
@@ -1,9 +1,18 @@
 import 'package:club_model/backend/navigation/navigation_arguments.dart';
+import 'package:club_model/models/user/data_model/user_model.dart';
 
 class OtpScreenNavigationArguments extends NavigationArguments {
   final String mobile;
 
   const OtpScreenNavigationArguments({
     required this.mobile,
+  });
+}
+
+class EditProfileScreenNavigationArguments extends NavigationArguments {
+  final UserModel userModel;
+
+  const EditProfileScreenNavigationArguments({
+    required this.userModel,
   });
 }
